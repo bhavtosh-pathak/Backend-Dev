ad25b5e5e58cfd540d1d8ea9e13e7927672b6afc
bhavtosh-pathak <bhavtoshpathak@gmail.com>
2026-01-19 23:24:05 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+import logActivity from "./logger.js";
+
+logActivity("hemant");
+ import http from "http";
+
+const server=http.createServer((req,res)=>{
+    res.writeHead(200,{"Content-Type": "application/json" });
+    res.end(JSON.stringify({ name: "hemant", age: 21 }));
+})
+
+
+server.listen(8000,()=>{
+    console.log("server is running on port 8000");
+})
